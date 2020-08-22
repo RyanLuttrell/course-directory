a9967872002403fdfaaeb1b3a74f0a7db0547311
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-22 07:50:46 -0600

    Updates

---
+++ precache-manifest.3b6222cd104d1c5f18eb3f02292d8730.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60736150e3f05f592202",
+    "url": "/course-directory/static/css/main.cc688b3a.chunk.css"
+  },
+  {
+    "revision": "60736150e3f05f592202",
+    "url": "/course-directory/static/js/main.60736150.chunk.js"
+  },
+  {
+    "revision": "25a97381f2fd20d18f35",
+    "url": "/course-directory/static/js/1.25a97381.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "6670070c331fe88475060d7cbf320fce",
+    "url": "/course-directory/index.html"
+  }
+];
